1e1a9e01fefc3c3df8a3aed3de32886ffa585fae
DSKT <diego99gu@hotmail.com>
2020-02-01 01:36:43 -0500

    Carpetas

---
+++ BlindPeace/Assets/Scripts/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+}
+[System.Serializable]
+public struct Next
+{
+    public 
+}
+
